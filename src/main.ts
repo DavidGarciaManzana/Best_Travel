24dec899963f6e9cfe8879b4142bbea54ede5dc7
David <Davidgarciama@outlook.com>
2023-02-08 13:06:14 -0600

    First advance

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-let chooseBestSum = (t: number, k: number, ls: number[]): number | null => {
+// let chooseBestSum = (t: number, k: number, ls: number[]): number | null => {
 
-    return 2
-}
+//     return 2
+// }
 
 
 // ts = [50, 55, 56, 57, 58] choose_best_sum(163, 3, ts) -> 163
@@ -13,8 +13,29 @@ let chooseBestSum = (t: number, k: number, ls: number[]): number | null => {
 //Example
 //ls = [50, 55, 57, 58, 60]. /3
 // [50,55,57],[50,55,58],[50,55,60],[50,57,58],[50,57,60],[50,58,60],[55,57,58],[55,57,60],[55,58,60],[57,58,60]
+
 //ls = [x, y, z, a, b]
-// [x,y,z],[x,y,a],[x,y,b],[x,z,a],[x,z,b],[x,a,b],
-// [y,z,a],[y,z,b],[y,a,b],
+// [x,y,z],[x,y,a],[x,y,b],
+// [x,z,a],[x,z,b],
+// [x,a,b],
+
+// [y,z,a],[y,z,b],
+// [y,a,b],
+
 // [z,a,b]
-//
+
+
+//1.-Create a function that does what you have on each of the lines above and call it for every letter in the array 
+//Stop when the the index of your base letter minus the lenght of the array is less than the k value (how many numbers in each group)
+
+let sumCombinations =(k: number,ls: number[])=>{
+    function myfunc (item:number, index:number){
+        if(index == 0) {
+            console.log(`item: ${item}, index: ${index}`)
+            console.log(item+ls[index+1])
+        }
+        
+    }
+    ls.forEach(myfunc);
+}
+sumCombinations(3,[50, 55, 57, 58, 60])
